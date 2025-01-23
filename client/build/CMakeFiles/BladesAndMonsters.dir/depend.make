9de5276bfc8842e21ce8efeeea7e7a1e04f62885
lordofgond0r <bwg.it6902@gmail.com>
2025-01-23 17:01:07 +0300

    [modify]

--- client/build/CMakeFiles/BladesAndMonsters.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BladesAndMonsters.
-# This may be replaced when dependencies are built.
--- client/build/CMakeFiles/BladesAndMonsters.dir/src/cDynamics/cDynamics.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/BladesAndMonsters.dir/src/cDynamics/cDynamics.cpp.o: \
- /home/kalex/projects/BladesAndMonsters/client/src/cDynamics/cDynamics.cpp \
- /usr/include/stdc-predef.h
