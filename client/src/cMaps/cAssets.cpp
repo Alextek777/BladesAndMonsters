d1a01429a23137822760e8aedf81918ea65cf379
lordofgond0r <bwg.it6902@gmail.com>
2025-01-30 00:56:45 +0300

    [MODIFY] changed animation for both sides

--- client/src/cMaps/cAssets.cpp
+++ client/src/cMaps/cAssets.cpp
@@ -13,14 +13,14 @@ Assets::~Assets()
 
 void Assets::LoadAnimations() {
 	m_animationHandler.load("witty", e_GraphicsState::WALKING, e_FactionDirection::EAST, 8, 100, "animations/witty/walking/east.png");
-	m_animationHandler.load("witty", e_GraphicsState::WALKING, e_FactionDirection::WEST, 9, 100, "animations/witty/walking/west.png");
+	m_animationHandler.load("witty", e_GraphicsState::WALKING, e_FactionDirection::WEST, 8, 100, "animations/witty/walking/west.png");
 	m_animationHandler.load("witty", e_GraphicsState::WALKING, e_FactionDirection::NORTH, 8, 100, "animations/witty/walking/east.png");
-	m_animationHandler.load("witty", e_GraphicsState::WALKING, e_FactionDirection::SOUTH, 8, 100, "animations/witty/walking/east.png");
+	m_animationHandler.load("witty", e_GraphicsState::WALKING, e_FactionDirection::SOUTH, 8, 100, "animations/witty/walking/west.png");
 
 	m_animationHandler.load("witty", e_GraphicsState::STANDING, e_FactionDirection::EAST, 6, 100, "animations/witty/standing/east.png");
-	m_animationHandler.load("witty", e_GraphicsState::STANDING, e_FactionDirection::WEST, 6, 100, "animations/witty/standing/east.png");
+	m_animationHandler.load("witty", e_GraphicsState::STANDING, e_FactionDirection::WEST, 6, 100, "animations/witty/standing/west.png");
 	m_animationHandler.load("witty", e_GraphicsState::STANDING, e_FactionDirection::NORTH, 6, 100, "animations/witty/standing/east.png");
-	m_animationHandler.load("witty", e_GraphicsState::STANDING, e_FactionDirection::SOUTH, 6, 100, "animations/witty/standing/east.png");
+	m_animationHandler.load("witty", e_GraphicsState::STANDING, e_FactionDirection::SOUTH, 6, 100, "animations/witty/standing/west.png");
 
 
 	m_animationHandler.load("TreeAutumn", e_GraphicsState::DEFAULT_GS, e_FactionDirection::DEFAULT_FD, 16, 64, "animations/trees/autumn/default.png");
