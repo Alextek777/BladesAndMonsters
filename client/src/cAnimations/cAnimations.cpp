facbf6cf5d7b5e908410b601d3374d9b862407e9
lordofgond0r <bwg.it6902@gmail.com>
2025-02-08 13:51:01 +0300

    [MODIFY]

--- client/src/cAnimations/cAnimations.cpp
+++ client/src/cAnimations/cAnimations.cpp
@@ -11,3 +11,45 @@ void cAnimationHandler::load(string name, e_GraphicsState state, e_FactionDirect
 AnimationFrame* cAnimationHandler::getAnimationFrame(string &name, e_GraphicsState state, e_FactionDirection direction, uint16_t counter) {
     return animations.at(name).at(state).at(direction).getFrame(counter);
 }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+
+Animation::Animation(uint16_t frameCount, uint16_t frameWidth, uint16_t frameHeight, olc::Sprite *sprite) {
+    frameVec = new AnimationFrame[frameCount];
+
+
+    this->sprite = sprite;
+    this->frameCount = frameCount;
+    this->frameWidth = frameWidth;
+    this->frameHeight = frameHeight;
+
+    olc::Decal* decal = new olc::Decal(sprite);
+    for (int i = 0; i < frameCount; i++) {
+        frameVec[i].decal = decal;
+        frameVec[i].ox = i * frameWidth;
+        frameVec[i].oy = 0;
+        frameVec[i].width = frameWidth;
+        frameVec[i].height = frameHeight;
+    }
+}
+
+AnimationFrame* Animation::getFrame(uint16_t counter) {
+    if (frameVec == nullptr || frameCount == 0 || &frameVec[counter % frameCount] == nullptr) {
+        throw std::out_of_range("Invalid frame access.");
+    }
+
+    return &frameVec[counter % frameCount];
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+AnimationFrame::AnimationFrame(olc::Sprite* sprite, uint16_t ox, uint16_t oy, uint16_t width, uint16_t height) {
+    this->decal = new olc::Decal(sprite);
+    this->ox = ox;
+    this->oy = oy; // TODO: might be unnececarry, all animations should start from upper corner
+    this->width = width;
+    this->height = height;
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------
--- client/src/cAnimations/cAnimations.h
+++ client/src/cAnimations/cAnimations.h
@@ -21,25 +21,6 @@ public:
 };
 
 
-struct AnimationFrame {
-    olc::Decal* decal;
-    uint16_t ox, oy;
-    uint16_t width;
-    uint16_t height;
-
-
-    AnimationFrame(){}
-
-    AnimationFrame(olc::Sprite* sprite, uint16_t ox, uint16_t oy, uint16_t width, uint16_t height) {
-        this->decal = new olc::Decal(sprite);
-        this->ox = ox;
-        this->oy = oy; // TODO: might be unnececarry, all animations should start from upper corner
-        this->width = width;
-        this->height = height;
-    }
-};
-
-
 struct Animation {
     olc::Sprite *sprite;
     uint16_t frameCount;
@@ -48,30 +29,16 @@ struct Animation {
     AnimationFrame* frameVec;
     Animation(){}
 
-    Animation(uint16_t frameCount, uint16_t frameWidth, uint16_t frameHeight, olc::Sprite *sprite) {
-        frameVec = new AnimationFrame[frameCount];
+    Animation(uint16_t frameCount, uint16_t frameWidth, uint16_t frameHeight, olc::Sprite *sprite);
+    AnimationFrame* getFrame(uint16_t counter);
+};
 
+struct AnimationFrame {
+    olc::Decal* decal;
+    uint16_t ox, oy;
+    uint16_t width;
+    uint16_t height;
 
-        this->sprite = sprite;
-        this->frameCount = frameCount;
-        this->frameWidth = frameWidth;
-        this->frameHeight = frameHeight;
-
-        olc::Decal* decal = new olc::Decal(sprite);
-        for (int i = 0; i < frameCount; i++) {
-            frameVec[i].decal = decal;
-            frameVec[i].ox = i * frameWidth;
-            frameVec[i].oy = 0;
-            frameVec[i].width = frameWidth;
-            frameVec[i].height = frameHeight;
-        }
-    }
-
-    AnimationFrame* getFrame(uint16_t counter) {
-        if (frameVec == nullptr || frameCount == 0 || &frameVec[counter % frameCount] == nullptr) {
-            throw std::out_of_range("Invalid frame access.");
-        }
-
-        return &frameVec[counter % frameCount];
-    }
+    AnimationFrame(){}
+    AnimationFrame(olc::Sprite* sprite, uint16_t ox, uint16_t oy, uint16_t width, uint16_t height);
 };
